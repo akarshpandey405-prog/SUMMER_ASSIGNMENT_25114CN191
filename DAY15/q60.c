0753e485599d9a5be9b78bad63927c4479e22e21
AKARSH PANDEY <akarshpandey405@gmail.com>
2026-06-14 17:35:57 +0530

    Add files via upload

---
+++ DAY15/q60.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int index = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[index++] = arr[i];
+        }
+    }
+
+    while(index < n) {
+        arr[index++] = 0;
+    }
+
+    printf("Array after moving zeros to end:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
